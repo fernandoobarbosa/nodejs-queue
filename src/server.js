9fc9082ecc6196f8a74fcefc45fcfc4efd1f249f
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2023-01-17 12:49:09 -0300

    feat: 🎸 bull UI

--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
+import {
+  ExpressAdapter,
+  createBullBoard,
+  BullAdapter,
+  BullMQAdapter,
+} from "@bull-board/express";
 
 import UserController from "./app/controllers/UserController";
+import Queue from "./app/lib/Queue";
 
 const app = express();
 
-app.use(express.json());
+const queues = Queue.queues.map((queue) => queue.bull);
+const bullQueues = queues.map((queue) => new BullAdapter(queue));
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath("/admin/queues");
 
+const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+  queues: bullQueues,
+  serverAdapter: serverAdapter,
+});
+
+app.use(express.json());
 app.post("/users", UserController.store);
+
+app.use("/admin/queues", serverAdapter.getRouter());
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
